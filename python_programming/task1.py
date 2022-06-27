6dc12576939c3861538d1b04227d1dfb4ca253d2
Raja Majumdar <raja.majumdar.01@gmail.com>
2022-06-27 13:48:02 +0530

    Create task1.py

---
+++ python_programming/task1.py
@@ -0,0 +1,60 @@
+import random
+import string
+
+sep = '-' * 30
+
+def level1():
+    print(sep + " LEVEL 1 " + sep)
+
+    FLAG = random.getrandbits(32)
+    ANSWER = bin(FLAG)[2:]
+
+    print("\nBinary representation of a number = %s" % ANSWER)
+    guess = int(input("Enter the number (decimal): "))
+
+    if guess == FLAG:
+        print("[+] Congratulations!!\n You passed level 1!")
+    else:
+        raise
+
+
+def level2():
+    print(sep + " LEVEL 2 " + sep)
+
+    FLAG = random.choice(string.ascii_lowercase)
+    ANSWER = bin(ord(FLAG))[2:]
+
+    print("\nBinary representation of a character = %s" % ANSWER)
+    guess = input("Enter the character: ").strip()
+
+    if guess == FLAG:
+        print("[+] Congratulations!!\n You passed level 2!")
+    else:
+        raise
+
+
+def level3():
+    FLAG = ''.join([random.choice(string.ascii_lowercase) for _ in range(6)])
+    ANSWER = ''.join([bin(ord(x))[2:].zfill(8) for x in FLAG])
+
+    print("\nBinary representation of a random FLAG = %s" % ANSWER)
+    guess = input("Enter the FLAG: ").strip()
+
+    if guess == FLAG:
+        print("\n[+] Congratulations!!\n You passed level 3!")
+    else:
+        raise
+
+
+if __name__ == '__main__':
+
+    FLAG = "flag{bit_by_bit}"
+
+    try:
+        level1()
+        level2()
+        level3()
+    except:
+        exit("\n[x] Oops! Try Again!")
+
+    print("\n[+] Submit the FLAG => %s" % FLAG)
